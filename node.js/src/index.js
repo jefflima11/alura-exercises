54f680df619f49d5c6187ebffe34e9dc45cc9b3f
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-11-19 12:52:22 -0300

    Update index.js

--- node.js/src/index.js
+++ node.js/src/index.js
@@ -4,5 +4,52 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8' , (erro, texto) => {
-    console.log(texto);
+    // verificaPalavrasDuplicadas(texto);
+    quebraEmParagrafos(texto);
 })
+
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+
+// {
+//     "web": 5,
+//     "computador": 4
+// }
+
+function quebraEmParagrafos(texto)  {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+}
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        if (palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras[palavra];
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+        }
+    })
+    return resultado;
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+      if (palavra.length >= 3) {
+        const palavraLimpa = limpaPalavras(palavra);
+        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+      }
+    })
+    return resultado;
+  }
