22d9f57eb40e78b5e2b88999eb79c56d6da5e077
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-11-26 09:48:43 -0300

    create herança

---
+++ JS-OO/herancaPrototipo.js
@@ -0,0 +1,29 @@
+const user = {
+    nome: "Juliana",
+    email: "j@j.com",
+    nascimento: "2024-01-01",
+    role: "estudante",
+    ativo: true,
+    exibirInfos: function () {
+        console.log(this.nome, this.email);
+    },
+    exibirTeste: function () {
+        console.log('apenas um teste de heranca');
+    }
+}
+
+const admin = {
+    nome: "Mariana",
+    email: "m@m.com",
+    nascimento: "2024-01-01",
+    role: "admin",
+    ativo: true,
+    criarCurso: function () {
+        console.log('curso criado');
+    }
+}
+
+Object.setPrototypeOf(admin, user);
+admin.criarCurso();
+admin.exibirInfos();
+admin.exibirTeste();
