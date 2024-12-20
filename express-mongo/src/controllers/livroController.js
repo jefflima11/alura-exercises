14339a3c0b7fe718d2a799867d225de703bd86be
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-12-20 17:59:35 -0300

    Add author model and routes for CRUD operations
    - Create Mongoose schema and model for authors
    - Implement routes for author CRUD operations
    - Add controller methods for handling author data
    - Integrate author routes into the main application

--- express-mongo/src/controllers/livroController.js
+++ express-mongo/src/controllers/livroController.js
@@ -1,20 +1,25 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
 
     static async listarLivros (req, res) {
         try {
-            const listaLivros = await livro.find();
+            const listaLivros = await livro.find({});
             res.status(200).json(listaLivros);
         } catch (erro) {
-            res.status(500).json({ message: `${erro.message} - flaha na requisição`});
+            res.status(500).json({ message: `${erro.message} - falha na requisição`});
         }
     }
 
     static async cadastrarLivro (req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
+            const livroCriado = await livro.create(livroCompleto);
+            res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro`})
         }
@@ -47,6 +52,16 @@ class LivroController {
             res.status(200).json({ message: "livro deletado"});
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao deletar`});
+        }
+    }
+
+    static async listarLivrosPorEditor (req, res) {
+        const editora = req.query.editora;
+        try {
+            const livrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livrosPorEditora);
+        } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na busca por editora`});
         }
     }
 
