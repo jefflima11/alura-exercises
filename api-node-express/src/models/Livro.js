b09fc18a623d8edd002205406ccafa32e857f82b
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-01-06 10:54:04 -0300

    tratamento de erros

--- api-node-express/src/models/Livro.js
+++ api-node-express/src/models/Livro.js
@@ -14,9 +14,21 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String, 
-      required: [true, "A editora do livro é obrigatória"]
+      required: [true, "A editora do livro é obrigatória"],
+        enum: {
+          values: ["Casa do Código", "Alura"],
+          message: "A editora {VALUE} não é um valor permitido."
+        }
     },
-    numeroPaginas: {type: Number}
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve ser entre 10 e 5000. Valor informado: {VALUE}"
+      }
+    }
   }
 );
 
