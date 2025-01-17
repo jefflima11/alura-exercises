f246a2acd2557f4d5a6bb61f3eff6c229d19343a
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-01-17 14:42:02 -0300

    update pessoas

--- nodejs-express-sequelize/src/controllers/Controller.js
+++ nodejs-express-sequelize/src/controllers/Controller.js
@@ -11,6 +11,32 @@ class Controller {
             return res.status(400);
         }
     }
+
+    async atualiza(req, res) {
+        const { id } = req.params;
+        const dadosAtualizado = req.body;
+        try {
+            const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizado, Number(id));
+            if (!foiAtualizado) {
+                return res.status(400).json({ message: 'Não foi possível atualizar o registro' });
+            }
+            return res.status(200).json({ message: 'Registro atualizado com sucesso' });
+        } catch(erro) {
+
+        }
+    }
+
+    async atualizaRegistro(dadosAtualizados, id) {
+        const ListaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+            where: { id: id }
+        });
+
+        if(ListaDeRegistrosAtualizados[0] === 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 module.exports = Controller;
--- nodejs-express-sequelize/src/services/Services.js
+++ nodejs-express-sequelize/src/services/Services.js
@@ -8,6 +8,18 @@ class Services {
     async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
     }
+
+    async atualizaRegistro(dadosAtualizados, id) {
+        const ListaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+            where: { id: id }
+        });
+
+        if(ListaDeRegistrosAtualizados[0] === 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 module.exports = Services;
